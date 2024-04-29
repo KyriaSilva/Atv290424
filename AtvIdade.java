7528f6b4c633588b134cf8af8c47c186bac59a0c
KyriaSilva <161315796+KyriaSilva@users.noreply.github.com>
2024-04-29 10:58:49 -0300

    Add files via upload

---
+++ AtvIdade.java
@@ -0,0 +1,31 @@
+package titio;
+
+import java.util.Scanner;
+
+public class AtvIdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        float idade = 0;
+		float qtdeIdade = 1;
+		float idadeTotal = 0;
+		
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.println("Informe a primeira idade para média: ");
+          idade = sc.nextFloat();
+		  idadeTotal += idade;
+			while (idade > 0) {
+				qtdeIdade += 1;
+				System.out.println("Informe a segunda idade para média: ");
+				idade = sc.nextFloat();
+				idadeTotal+= idade;
+	            float mediaIdade =  idadeTotal / qtdeIdade;
+	            System.out.println("------------------------------------------------------------");
+				System.out.println("Média das idades: " + mediaIdade);
+				System.out.println("------------------------------------------------------------");
+			}
+		System.out.println("Fim da execução");
+	}
+
+}
